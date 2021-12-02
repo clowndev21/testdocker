989e8c42b5a4226affc2f4df8b8a010039213149
Binay kumar Pradhan <67338124+pbinaykumar@users.noreply.github.com>
2021-12-02 13:53:27 +0530

    ok

--- django_project/django_project/asgi.py
+++ django_project/django_project/asgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
-django.setup()
+
 application = get_asgi_application()
